81a7b89c0ef2afea62b048fac542a68cf45e2cf2
abderrahmaneelhamel <abdoelhamel359@gmail.com>
2023-12-05 14:17:28 +0100

    popup

--- src/app/components/popup/popup.component.ts
+++ src/app/components/popup/popup.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-popup',
   templateUrl: './popup.component.html',
   styleUrls: ['./popup.component.css']
 })
-export class PopupComponent {
-
+export class PopupComponent implements OnInit {
   hidden : boolean = true;
+  @Input() color!: string;
+  @Input() name!: string;
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  constructor(){}
 
   Toggle(){
     this.hidden = !this.hidden;
